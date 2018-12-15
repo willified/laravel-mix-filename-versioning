30bb06aea99052245354d1f4747957f973fe1806
William Huang <willified@users.noreply.github.com>
2018-12-14 17:28:31 -0800

    Update index.js

--- index.js
+++ index.js
@@ -5,15 +5,14 @@ class LaravelMixFilenameVersioning {
   apply (compiler) {
     compiler.plugin('done', function(stats) {
       const newAssets = {};
-
       Object.keys(stats.compilation.assets).forEach(assetName => {
-        let originalAssetNameParts = path.parse(assetName);
-        let newAssetFile = new File(path.join(Config.publicPath, assetName).replace(/\\/g, '/')); 
+		let fixedAssetName = assetName.replace(/\\/g, '/');
+        let originalAssetNameParts = path.parse(fixedAssetName.replace(/\\/g, '/'));
+        let newAssetFile = new File(path.join(Config.publicPath, fixedAssetName).replace(/\\/g, '/')); 
         let newAssetFileName = newAssetFile.segments.name + '.' + newAssetFile.version().substr(0, 8) + newAssetFile.segments.ext;
-
         newAssetFile.rename(newAssetFileName.replace(/\\/g, '/'));
 
-        let newAssetKeyName = path.join(originalAssetNameParts.dir, newAssetFileName);
+        let newAssetKeyName = path.join(originalAssetNameParts.dir, newAssetFileName).replace(/\\/g, '/');
         let newAssetFullFileName = path.join(newAssetFile.segments.base, newAssetFileName);
         newAssets[newAssetKeyName.replace(/\\/g, '/')] = stats.compilation.assets[assetName];
 
@@ -28,8 +27,7 @@ class LaravelMixFilenameVersioning {
         newAssets[newAssetKeyName.replace(/\\/g, '/')].size = function (assetAbsolutePath) {
           return new File(assetAbsolutePath).size();
         }.bind(null, newAssetFullFileName);
-
-        Mix.manifest.manifest[assetName] = newAssetKeyName.replace(/\\/g, '/');
+        Mix.manifest.manifest[fixedAssetName] = newAssetKeyName.replace(/\\/g, '/');
       });
 
       Mix.manifest.refresh();
